9fe15ad2a59fa1ef9de9e08663b540eddbf37623
suzukimain <131413573+suzukimain@users.noreply.github.com>
2025-04-28 16:40:24 +0900

    update

--- scripts/ui.py
+++ scripts/ui.py
@@ -1,9 +1,22 @@
 import gradio as gr
 
+
+from config import quickly_search_civitai
 from modules import script_callbacks
 
 
 tabs_list = ["checkpoint", "textual inversion", "Lora", "controlnet"]
+
+
+def ui_checkpoint():
+    with gr.Blocks() as demo:
+        with gr.Row():
+            textbox = gr.Textbox(label="search box", placeholder="please input your search word")
+            suggestion = gr.HTML(label="candidate")
+    
+        textbox.change(fn=quickly_search_civitai, inputs=textbox, outputs=suggestion)
+    
+    return demo
 
 
 def on_ui_tabs():
@@ -12,8 +25,11 @@ def on_ui_tabs():
             for tab in tabs_list:
                 with gr.Tab(tab):
                     with gr.Blocks(analytics_enabled=False) :
-                        search_button = gr.Button(f"Search {tab}")
-        
+                        if not tab == "checkpoint":
+                            search_button = gr.Button(f"Search {tab}")
+                        else:#ui_checkpoint()
+                            search_button = ui_checkpoint()
+
     return (search_ui , "Search", "auto_sd_webui"),
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
