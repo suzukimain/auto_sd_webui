75bac0a87e606e214b7afe50434ddd21c4dcaedd
suzukimain <131413573+suzukimain@users.noreply.github.com>
2025-04-28 15:08:52 +0900

    ui test

---
+++ scripts/ui.py
@@ -0,0 +1,19 @@
+import gradio as gr
+
+from modules import script_callbacks
+
+
+tabs_list = ["checkpoint", "textual inversion", "Lora", "controlnet"]
+
+
+def on_ui_tabs():
+    with gr.Blocks(analytics_enabled=False) as search_ui:
+        with gr.Tabs(elem_id="search_tab"):
+            for tab in tabs_list:
+                with gr.Tab(tab):
+                    with gr.Blocks(analytics_enabled=False) :
+                        search_button = gr.Button(f"Search {tab}")
+        
+    return (search_ui , "Search", "auto_sd_webui"),
+
+script_callbacks.on_ui_tabs(on_ui_tabs)
