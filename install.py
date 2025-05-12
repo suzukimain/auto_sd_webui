6d34f932b4e184e2fe9e15e8b11d13c7b81ce55d
suzukimain <131413573+suzukimain@users.noreply.github.com>
2025-05-12 17:18:22 +0900

    Create install.py

---
+++ install.py
@@ -0,0 +1,16 @@
+import subprocess
+import sys
+import re
+
+_deps = [
+    "auto_diffusers>=2.0.22",
+    "transformers<=4.32.1",
+]
+
+deps = {b: a for a, b in (re.findall(r"^(([^!=<>~]+)(?:[!=<>~].*)?$)", x)[0] for x in _deps)}
+
+def install_dependencies(deps):
+    for package in deps.values():
+        subprocess.run([sys.executable, "-m", "pip", "install", package])
+
+install_dependencies(deps)
